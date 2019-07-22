4e42f72da1c3f989e36343c640fd1e27889b2ced
maxipom <RAmones54>
2019-07-22 11:29:11 +0200

    Created an interceptor for production

--- back-end/schedule-api/app.js
+++ back-end/schedule-api/app.js
@@ -6,13 +6,13 @@ const schedule = require('./routes/schedule');
 const students = require('./routes/students');
 const classrooms = require('./routes/classrooms');
 const classes = require('./routes/classes');
-
+const cors = require('cors');
 const app = express();
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
 
-console.log('App.js on!');
+console.log('Schedule server is on!');
 app.use('/api/teachers', teachers);
 app.use('/api/schedule', schedule);
 app.use('/api/students', students);
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,28 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
 import {RouterModule, Routes} from '@angular/router';
-import { SearchBarComponent } from './search-bar/search-bar.component';
+import {SearchBarComponent} from './search-bar/search-bar.component';
 import {ReactiveFormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ScheduleComponent } from './schedule/schedule.component';
-import { CardComponent } from './card/card.component';
-import { TeacherContentCardComponent } from './card/content-cards/teacher-content-card/teacher-content-card.component';
-import { TeacherSearchComponent } from './search-bar/teacher-search/teacher-search.component';
-import { StudentSearchComponent } from './search-bar/student-search/student-search.component';
-import { StudentContentCardComponent } from './card/content-cards/student-content-card/student-content-card.component';
-import { ClassroomSearchComponent } from './search-bar/classroom-search/classroom-search.component';
-import { ClassroomContentCardComponent } from './card/content-cards/classroom-content-card/classroom-content-card.component';
-import { ClassSearchComponent } from './search-bar/class-search/class-search.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {NavbarComponent} from './navbar/navbar.component';
+import {ScheduleComponent} from './schedule/schedule.component';
+import {CardComponent} from './card/card.component';
+import {TeacherContentCardComponent} from './card/content-cards/teacher-content-card/teacher-content-card.component';
+import {TeacherSearchComponent} from './search-bar/teacher-search/teacher-search.component';
+import {StudentSearchComponent} from './search-bar/student-search/student-search.component';
+import {StudentContentCardComponent} from './card/content-cards/student-content-card/student-content-card.component';
+import {ClassroomSearchComponent} from './search-bar/classroom-search/classroom-search.component';
+import {ClassroomContentCardComponent} from './card/content-cards/classroom-content-card/classroom-content-card.component';
+import {ClassSearchComponent} from './search-bar/class-search/class-search.component';
+import {ProxyInterceptor} from './interceptors/proxy.interceptor';
+
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
   },
 ];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -45,7 +48,10 @@ const routes: Routes = [
     RouterModule.forRoot(routes),
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ProxyInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
